a03622847a9cc9735e92d06a615b4ba3d557b632
ogokdas <ogokdas@yandex.com>
2022-02-04 15:19:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int c = a + b;
+
+                Console.WriteLine(c);
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Lütfen sadece sayı yazınız." + Environment.NewLine + Ex.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
